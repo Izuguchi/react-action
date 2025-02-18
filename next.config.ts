dea15e6850c4311bc33ab849033fb052e13fa9ae
Lucas K <kenji_mizuguchi@outlook.com>
2025-02-18 23:57:28 +0000

    Validando alteracoes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/react-action',
 };
 
 export default nextConfig;
